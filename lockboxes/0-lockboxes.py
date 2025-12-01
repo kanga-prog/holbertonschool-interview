73c7c8f7e42c30425a171c3a2fc0ed01c40f2943
kanga-prog <9952@holbertonstudents.com>
2025-12-01 17:03:26 +0100

    Returns True if all boxes can be opened, else False

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -11,32 +11,19 @@ Requirements:
 """
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-
-    Args:
-        boxes (list of list of int): List of boxes, each containing keys
-
-    Returns:
-        bool: True if all boxes can be opened, else False
-    """
-    if not boxes or type(boxes) is not list:
+    if not boxes or type(boxes) != list:
         return False
 
     n = len(boxes)
-    opened = set([0])      # box 0 is initially opened
-    keys = set(boxes[0])   # keys from the first box
+    visited = [False] * n
 
-    while True:
-        opened_before = len(opened)
+    def dfs(box):
+        if visited[box]:
+            return
+        visited[box] = True
+        for key in boxes[box]:
+            if 0 <= key < n:
+                dfs(key)
 
-        for key in list(keys):
-            if 0 <= key < n and key not in opened:
-                opened.add(key)
-                keys.update(boxes[key])
-
-        # Stop if no new boxes were opened
-        if len(opened) == opened_before:
-            break
-
-    return len(opened) == n
+    dfs(0)
+    return all(visited)
